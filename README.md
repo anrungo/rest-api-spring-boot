6919b7e5c66127ba7345bdc60fab2ad7ee410ee9
Antonio Rungo <tony.niquisse@gmail.com>
2021-06-02 13:05:21 +0200

    adding files to github

---
+++ README.md
@@ -0,0 +1,8 @@
+<h2>Sistema de gerenciamento de pessoas em API REST com Spring Boot</h2> 
+
+Neste prohecto desenvolvemos um sistema para o gerenciamento de pessoas de uma empresa através de uma API REST, criada com o Spring Boot. :globe_with_meridians:
+
+
+
+
+
---
+++ src/main/java/one/digitalinnovation/personrestapi/PersonrestapiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.personrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonrestapiApplication.class, args);
+	}
+
+}
